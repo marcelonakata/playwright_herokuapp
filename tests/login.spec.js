0158a709f9ae59b710ec170f4d9a225bc75cf7b6
Marcelo <marceloynakata@gmail.com>
2023-06-22 12:01:11 -0700

    Added test.describe block

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,27 +1,29 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/login');
-});
+test.describe('Herokuapp Login Test Suite', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/login');
+  });
 
-test('Test', async ({ page }) => {
+  test('Test', async ({ page }) => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("The Internet");
-});
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle("The Internet");
+  });
 
-test('Verify URL after succesful login', async ({ page }) => {
+  test('Verify URL after succesful login', async ({ page }) => {
 
-  // Type username.
-  await page.locator('#username').type("tomsmith");
+    // Type username.
+    await page.locator('#username').type("tomsmith");
 
-  // Type password.
-  await page.locator('#password').type("SuperSecretPassword!");
+    // Type password.
+    await page.locator('#password').type("SuperSecretPassword!");
 
-  // Click on 'Login' butto.
-  await page.locator('xpath=//*[@id="login"]/button/i').click();
+    // Click on 'Login' butto.
+    await page.locator('xpath=//*[@id="login"]/button/i').click();
 
-  // Expects the URL to contain 'secure'.
-  await expect(page).toHaveURL(/secure/);
+    // Expects the URL to contain 'secure'.
+    await expect(page).toHaveURL(/secure/);
+  });
 });
